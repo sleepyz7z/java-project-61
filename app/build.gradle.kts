685cd2c46a49919f083bf0799cb0565c008b9da3
Igor Kushvid <kushvidis@mail.ru>
2025-06-02 01:35:24 -0700

    Третий и четвертый шаг

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("java")
     id("com.github.ben-manes.versions") version "0.47.0"
     application
+    checkstyle
 }
 
 group = "hexlet.code"
@@ -9,6 +10,16 @@ version = "1.0-SNAPSHOT"
 
 application {
     mainClass.set("hexlet.code.App")
+}
+
+checkstyle {
+    toolVersion = "10.12.1"  // Актуальная версия
+    configFile = file("config/checkstyle/checkstyle.xml")
+    isIgnoreFailures = false  // Фатальная ошибка при нарушениях
+}
+
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
 }
 
 repositories {
